eff5b7fd47590690d9714253d64fd168816aa94b
Sudip Adhikari <sadhikaris2016@gmail.com>
2022-01-11 23:43:12 -0600

    first commmit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int value =4;
+        switch(value){
+            case 1:
+                System.out.println("value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            case 3:
+                System.out.println("value was 3");
+                break;
+            case 4:
+                System.out.println("This was 4");
+
+            case 5:
+                System.out.println("this is was 5");
+            case 6:
+                System.out.println("this is case 6");
+                break;
+
+        }
+
+
+
+    }
+}
