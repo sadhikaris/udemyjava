c9465efb80683d74f7487dfb5c468c8ccef43486
Sudip Adhikari <sadhikaris2016@gmail.com>
2022-01-12 11:41:23 -0600

    second update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        int value =4;
+        int value =5;   // this works as if statement
         switch(value){
             case 1:
                 System.out.println("value was 1");
@@ -25,9 +25,62 @@ public class Main {
                 System.out.println("this is case 6");
                 break;
 
+            default:  // this is for something that has not covered above
+                System.out.println("this is default");
+                break;
+
         }
 
-
+    printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(4);
 
     }
+
+    public static void printDayOfTheWeek(int day){
+
+        switch(day){
+            case 1:
+                System.out.println("Today is Sunday");
+                break;
+            case 2:
+                System.out.println("today is Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("wed");
+                break;
+            case 5:
+                System.out.println("thursday");
+                break;
+            case 6:
+                    System.out.println("friday");
+                    break;
+            case 7:
+                System.out.println("saturday");
+
+                break;
+            default:
+                System.out.println("INVALID DAY");
+                break;
+
+        }
+
+// this is for using if then statement
+
+
+if (day ==1) {
+    System.out.println("sunday");
+} else if (day == 2){
+    System.out.println("monday");  // follow the same pattern for other days
+} else {
+    System.out.println("invalid day");
 }
+    }
+
+
+}
+//  push this code on git@ initially done on 1/11/2022
+//cant fixed the gitignore issue
